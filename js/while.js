02c23233397751c5ecdc04f1e1957e39e83f3124
Byron Praetor <byronpc18@icloud.com>
2023-10-19 16:55:12 -0500

    DonE

--- js/while.js
+++ js/while.js
@@ -5,9 +5,7 @@ while (i<65588) {
 }
 
 // Do while
-// This is how you get a random number between 50 and 100
 let allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
 do{
     let conesBought = Math.floor(Math.random() * 5) + 1;
     if(conesBought<allCones){
