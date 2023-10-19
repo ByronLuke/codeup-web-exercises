89cec2ba018dd6525a2a4a07f367483032da5b29
Byron Praetor <byronpc18@icloud.com>
2023-10-19 14:07:04 -0500

    1-5 done on loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,6 @@
-let number = 7;
+let number = 1;
 
+//Question 2
 function showMultiplicationTable(number){
     for (let i = 1; i < 11; i++) {
         const result = number*i;
@@ -7,3 +8,31 @@ function showMultiplicationTable(number){
     }
 }console.log(showMultiplicationTable(number))
 
+//Question 3
+function evenOrOdd(){
+    for (let i = 0; i < 10; i++) {
+        let randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+        if(randomNumber%2===0){
+            console.log(`${randomNumber} is even!`)
+        }
+        else{
+            console.log(`${randomNumber} is odd!`)
+        }
+    }
+}
+console.log(evenOrOdd());
+
+//Question 4
+for (let i = 1; i <=9 ; i++) {
+        let result ='';
+        result += i.toString().repeat(i);
+        console.log(result)
+    }
+
+//Question 5
+
+for (let i = 100; i > 0 ; i--) {
+    if(i%5===0){
+        console.log(i)
+    }
+}
