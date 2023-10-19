81e2a69116c7c10ddc8c1c08be094f046f2ca852
Byron Praetor <byronpc18@icloud.com>
2023-10-19 16:11:36 -0500

    Final do while loop completed

--- js/while.js
+++ js/while.js
@@ -4,3 +4,20 @@ while (i<65588) {
     i = i*2;
 }
 
+// Do while
+// This is how you get a random number between 50 and 100
+let allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+do{
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+    if(conesBought<allCones){
+        console.log(`${conesBought} cones bought`)
+        allCones-=conesBought;
+    }
+    else if(conesBought>allCones){
+        console.log(`Cannot sell you ${conesBought} cones. I only have ${allCones} left`)
+    }else{
+        console.log("Yay! I sold them all!")
+        break;
+    }
+}while(allCones>0)
