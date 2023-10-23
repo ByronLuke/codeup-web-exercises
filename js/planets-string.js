0c444b15fc8615c2d29132a1044801e099257ac9
Byron Praetor <byronpc18@icloud.com>
2023-10-23 14:14:31 -0500

    planets-string.js completed

--- js/planets-string.js
+++ js/planets-string.js
@@ -10,18 +10,22 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+    planetsArray = planetsString.split('|')
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
 
+    var planetsWithBreaks = planetsArray.join('<br>');
+    console.log(planetsWithBreaks);
+
+    var planetsWithUnorderedList = '<ul><li>' + planetsArray.join('<li>') +'<li><ul>'
+    console.log(planetsWithUnorderedList);
 })();
