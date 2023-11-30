845956efc5477e1c91e1c399899b5087581b9a22
Byron Praetor <byronpc18@icloud.com>
2023-11-30 11:00:36 -0600

    keys added

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,28 +1,36 @@
+//grab the divvy
 const div = document.querySelector('div');
 
+//fetch the data
 const data = fetch('data/blog.json', {
     method: 'GET'
 }).
 then(response=> response.json()
+    //what we want to happen with that
     .then(data => {
         data.forEach(dataObj=>{
+            //create the section
             const section = document.createElement('section');
 
+            //create all the elements for future use and attachment
             const titleData = document.createElement('h1');
             const contentData = document.createElement('p');
             const categoryData = document.createElement('p');
             const dateData = document.createElement('p');
 
+            //add the text content from the object so it ain't blank
             titleData.textContent = dataObj.title;
             contentData.textContent = dataObj.content;
-            categoryData.textContent = dataObj.content;
+            categoryData.textContent = dataObj.categories;
             dateData.textContent = dataObj.date;
 
+            //section appendy
             section.appendChild(titleData)
             section.appendChild(contentData)
             section.appendChild(categoryData)
             section.appendChild(dateData)
 
+            //divvy appendy
             div.appendChild(section)
         })
     }))
---
+++ js/keys.js
@@ -0,0 +1,2 @@
+const MB_KEY = 'pk.eyJ1IjoiYnlyb25wYzE4IiwiYSI6ImNscGxmdnV6cDAyMjIya281NjRyNDA3ZngifQ.XGvGdaN7gbMn3wlilWWlew'
+const GH_KEY ='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';
