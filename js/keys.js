daa674ba2ce4f87f13b10d9c07b618fb5a43efab
Byron Praetor <byronpc18@icloud.com>
2023-12-01 11:05:40 -0600

    html created and key accessed

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,3 @@
 const MB_KEY = 'pk.eyJ1IjoiYnlyb25wYzE4IiwiYSI6ImNscGxmdnV6cDAyMjIya281NjRyNDA3ZngifQ.XGvGdaN7gbMn3wlilWWlew'
 const GH_KEY ='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';
+const WM_KEY = '1ce928eb8eeebda1de1822fe91145c27';
